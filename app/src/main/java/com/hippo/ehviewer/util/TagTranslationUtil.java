d31eaf8570eff7a67de1bd3bcb5c9345444ffc90
杰神_杰影 <xiaojieonly@foxmail.com>
2021-10-27 00:32:49 +0800

    2021/10/27/00:19 翻译文件更新

---
+++ app/src/main/java/com/hippo/ehviewer/util/TagTranslationUtil.java
@@ -0,0 +1,71 @@
+package com.hippo.ehviewer.util;
+
+import android.content.res.AssetManager;
+
+
+import com.hippo.ehviewer.client.EhTagDatabase;
+
+
+public class TagTranslationUtil {
+
+    public static String getTagCN(String[] tags, EhTagDatabase ehTags) {
+        if (ehTags != null && tags.length == 2) {
+            String group = ehTags.getTranslation("n:" + tags[0]);
+            //翻译标签名
+            String prefix = EhTagDatabase.namespaceToPrefix(tags[0]);
+            String tagStr = ehTags.getTranslation(prefix != null ? prefix + tags[1] : "" + tags[1]);
+
+
+            if (group != null && tagStr != null) {
+                return group + "：" + tagStr;
+            } else if (group != null && tagStr == null) {
+                return group + "：" + tags[1];
+            } else if (group == null && tagStr != null) {
+                return tags[0] + "：" + tagStr;
+            } else {
+                return tags[0] + ":" + tags[1];
+            }
+        } else {
+            StringBuffer s = new StringBuffer();
+            for (int i = 0; i < tags.length; i++) {
+                if (i == 0) {
+                    s = s.append(tags[i]);
+//                    s +=  tags[i];
+                } else {
+                    s = s.append(":").append(tags[i]);
+//                    s +=  ":" + tags[i];
+                }
+            }
+
+            return s.toString();
+        }
+    }
+
+    public static String getTagCNBody(String[] tags, EhTagDatabase ehTags) {
+        if (ehTags != null && tags.length == 2) {
+            String group = ehTags.getTranslation("n:" + tags[0]);
+            //翻译标签名
+            String prefix = EhTagDatabase.namespaceToPrefix(tags[0]);
+            String tagstr = ehTags.getTranslation(prefix != null ? prefix + tags[1] : "" + tags[1]);
+
+
+            if (group != null && tagstr != null) {
+                return tagstr;
+            } else if (group != null && tagstr == null) {
+                return tags[1];
+            } else if (group == null && tagstr != null) {
+                return tagstr;
+            } else {
+                return tags[1];
+            }
+        } else {
+            StringBuffer s = new StringBuffer();
+            for (String i : tags) {
+                s = (new StringBuffer()).append(i);
+            }
+            return s.toString();
+        }
+    }
+
+
+}
