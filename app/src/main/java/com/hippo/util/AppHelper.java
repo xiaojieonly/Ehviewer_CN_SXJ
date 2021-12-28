51de9c25bbae5ff6dfba6c7450b8fc1855833c65
杰神_杰影 <xiaojieonly@foxmail.com>
2021-12-28 19:11:45 +0800

    1.8.10.* : 布局文件提交，配置文件提交，部分代码优化

--- app/src/main/java/com/hippo/util/AppHelper.java
+++ app/src/main/java/com/hippo/util/AppHelper.java
@@ -19,6 +19,8 @@ package com.hippo.util;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.Service;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -95,5 +97,19 @@ public class AppHelper {
             InputMethodManager imm = (InputMethodManager) dialog.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
+    }
+
+    public static void copyPlainText(String data,Context context) {
+        // 获取系统剪贴板
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        // 创建一个剪贴数据集，包含一个普通文本数据条目（需要复制的数据）,其他的还有
+        // newHtmlText、
+        // newIntent、
+        // newUri、
+        // newRawUri
+        ClipData clipData = ClipData.newPlainText(null, data);
+
+        // 把数据集设置（复制）到剪贴板
+        clipboard.setPrimaryClip(clipData);
     }
 }
