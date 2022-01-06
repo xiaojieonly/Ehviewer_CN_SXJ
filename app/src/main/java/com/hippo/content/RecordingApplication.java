71334072d9efea24beed6f273ac52d9158ad20ee
杰神_杰影 <xiaojieonly@foxmail.com>
2022-01-06 18:37:47 +0800

    应用类更新

--- app/src/main/java/com/hippo/content/RecordingApplication.java
+++ app/src/main/java/com/hippo/content/RecordingApplication.java
@@ -56,17 +56,21 @@ public abstract class RecordingApplication extends SceneApplication {
         }
       }
 
-      @Override
-      public void onActivityStarted(Activity activity) {}
+            @Override
+            public void onActivityStarted(Activity activity) {
+            }
 
-      @Override
-      public void onActivityResumed(Activity activity) {}
+            @Override
+            public void onActivityResumed(Activity activity) {
+            }
 
-      @Override
-      public void onActivityPaused(Activity activity) {}
+            @Override
+            public void onActivityPaused(Activity activity) {
+            }
 
-      @Override
-      public void onActivityStopped(Activity activity) {}
+            @Override
+            public void onActivityStopped(Activity activity) {
+            }
 
       @Override
       public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
