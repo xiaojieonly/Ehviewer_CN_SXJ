955882eca919e521ee0ea964768ca5e66900bf0a
杰神_杰影 <xiaojieonly@foxmail.com>
2022-01-23 23:00:39 +0800

    修复一个可能因空安全导致的程序奔溃bug

--- app/src/main/java/com/hippo/network/EhSSLSocketFactory.java
+++ app/src/main/java/com/hippo/network/EhSSLSocketFactory.java
@@ -27,7 +27,10 @@ public class EhSSLSocketFactory extends SSLSocketFactory {
             return ((SSLSocketFactory) getDefault()).createSocket(s, host, port, autoClose);
         }
         InetAddress address = s.getInetAddress();
-        Log.d("EhSSLSocketFactory", "Host: " + host + " Address: " + address.getHostAddress());
+        String hostAddress = address.getHostAddress();
+        if (hostAddress != null){
+            Log.d("EhSSLSocketFactory", "Host: " + host + " Address: " + hostAddress);
+        }
         if (autoClose) s.close();
         return getDefault().createSocket(address, port);
     }
