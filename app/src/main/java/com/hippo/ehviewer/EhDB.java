a94ce486f7771f1f7856bc583ff53375baf0a34b
shuaixiaojie <1031082091@qq.com>
2025-11-06 14:15:38 +0800

    fix: downgrade database version to 6

--- app/src/main/java/com/hippo/ehviewer/EhDB.java
+++ app/src/main/java/com/hippo/ehviewer/EhDB.java
@@ -177,7 +177,7 @@ public class EhDB {
     private static class OldDBHelper extends SQLiteOpenHelper {
 
         private static final String DB_NAME = "data";
-        private static final int VERSION = 7;
+        private static final int VERSION = 6;
 
         private static final String TABLE_GALLERY = "gallery";
         private static final String TABLE_LOCAL_FAVOURITE = "local_favourite";
