4de490d43bd97113773935020166caba1054461b
Mamba_out <1031082091@qq.com>
2026-03-18 15:58:02 +0800

    fix(sign): 修正登录WebView客户端设置及资料获取逻辑

--- app/src/main/java/com/hippo/ehviewer/ui/scene/sign/SignInScene.java
+++ app/src/main/java/com/hippo/ehviewer/ui/scene/sign/SignInScene.java
@@ -214,7 +214,7 @@ public final class SignInScene extends SolidScene implements EditText.OnEditorAc
                 getProfile();
             }
         } else if (requestCode == REQUEST_CODE_PROFILE) {
-            if (RESULT_OK == resultCode && data != null) {
+            if (data != null) {
                 getProfileSuccess(data);
             } else {
                 // 用户取消或获取资料失败，结束登录流程并隐藏进度
