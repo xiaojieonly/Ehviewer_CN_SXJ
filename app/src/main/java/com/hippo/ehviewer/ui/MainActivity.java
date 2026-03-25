03685b260b682def99cb276a87c5bd19310c8943
mikisayaga <meishushayaxiang366@gmail.com>
2026-03-25 16:48:22 +0800

    修改了主题切换问题，现在主题切换由原来的跟随系统主题变成了跟随app内部设置，还待解决搜索问题

--- app/src/main/java/com/hippo/ehviewer/ui/MainActivity.java
+++ app/src/main/java/com/hippo/ehviewer/ui/MainActivity.java
@@ -49,7 +49,10 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//补一下，不然编译不通过
+import android.os.Build;
+import android.os.Environment;
+//
 import androidx.annotation.IdRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
