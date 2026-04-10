072f2cc08451b0d1b58798bf510ce02bca71b19c
Cololi <10243794+cololi@users.noreply.github.com>
2026-04-10 14:57:13 +0800

    撤销gradle.bat的修改

--- gradlew.bat
+++ gradlew.bat
@@ -75,6 +75,7 @@ set CLASSPATH=
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+
 :end
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
