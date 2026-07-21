2253119f323e26b8f6aeaf2a71f756cff0ae73dd
PegionFish <boblao0714@gmail.com>
2026-07-21 14:25:18 +0800

    chore: remove unused smbj dependency from ehviewer-core

--- ehviewer-core/build.gradle.kts
+++ ehviewer-core/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:3.14.7")
     implementation("org.jsoup:jsoup:1.15.4")
     implementation("org.ccil.cowan.tagsoup:tagsoup:1.2.1")
-    implementation("com.hierynomus:smbj:0.12.0")
     implementation("com.alibaba:fastjson:1.2.83")
     implementation("org.json:json:20231013")
     compileOnly("org.slf4j:slf4j-api:2.0.9")
